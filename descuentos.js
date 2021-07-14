fecfc908ed7ca451824a3681d50e9544f145ea50
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2021-07-14 12:52:11 -0500

    JS, estilos y header terminados de descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,7 +1,10 @@
 const precio = document.querySelector("#product_price");
 const descuento = document.querySelector("#product_discount");
-const result = document.querySelector("#resultado")
-const result_description = document.querySelector("#descripcion")
+const result = document.querySelector("#resultado");
+const result_description = document.querySelector("#descripcion");
+const cupon_input = document.querySelector("#cupon");
+const CUPON = '$Pr3mio';
+const DESCUENTO_CUPON = 10;
 
 const priceWithDiscount = (precio, porcentaje) => {
     let resultado = precio * (100 - porcentaje) / 100;
@@ -9,9 +12,21 @@ const priceWithDiscount = (precio, porcentaje) => {
 }
 
 function calcularPrecio() {
-    let precio_value = precio.value;
-    let discount_value = descuento.value;
-    let resultado = priceWithDiscount(precio_value, discount_value);
-    result.innerHTML = `Precio: $${resultado}.` ;
-    result_description.innerHTML =  `Descuento del ${discount_value}%`;
+    let precio_value = Number(precio.value);
+    let discount_value = Number(descuento.value);
+    let user_cupon = cupon_input.value;
+    let resultado;
+    if(user_cupon === CUPON){
+        let descuento_extra = discount_value + DESCUENTO_CUPON;
+        resultado = priceWithDiscount(precio_value, descuento_extra)
+        result_description.innerHTML = `Se ha aplicado un descuento del ${descuento_extra}%. ${discount_value}% 
+        del descuento normal más ${DESCUENTO_CUPON}% del cupón`
+    }else {
+        resultado = priceWithDiscount(precio_value, discount_value);
+        result_description.innerHTML = 'El código de tu cupón no es válido'
+
+    }
+    result.innerHTML = `Precio: $${resultado}.`;
+    /* let resultado = priceWithDiscount(precio_value, discount_value);
+    result.innerHTML = `Precio: $${resultado}.`; */
 }
